7c6f776987de6cb5f04add5c485540a14a4e8dd0
Jean Fonseca De Aquino <jean.aquino@rededor.com.br>
2020-09-06 16:42:30 -0300

    Corrigindo bug do export svg

--- storybookMobile/src/components/atoms/index.js
+++ storybookMobile/src/components/atoms/index.js
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Colors } from './Colors';
 export { default as Input } from './Input';
 export { default as Label } from './Label';
+export { default as Svg } from './Svg';
