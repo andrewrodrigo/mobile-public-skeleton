4761b87fc8625d880f371de476fbdde78c28165b
Jean Fonseca De Aquino <jean.aquino@rededor.com.br>
2020-10-19 05:33:59 -0300

    Incluindo arquivos de sprites

--- storybookMobile/src/components/atoms/ScrollContent/index.js
+++ storybookMobile/src/components/atoms/ScrollContent/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { ScrollView } from 'react-native'
+import Colors from '../Colors';
 
 const ScrollContent = (props) => {
 
-    const { children } = props;
+    const { children, primary, secondary, white } = props;
+    const color = (primary, secondary, white) =>{
+        if(primary){
+            return Colors.primary.defaultHeightOpacity;
+        }
+        if(secondary){
+            return Colors.secondary.defaultHeightOpacity;
+        }
+        if(white){
+            return Colors.globals.bg;
+        }
+        return 'transparent';
+    }
+    const selectedColor = color(primary, secondary, white);
 
     return (
-        <ScrollView>{children}</ScrollView>
+        <ScrollView style={{ backgroundColor: selectedColor}}>{children}</ScrollView>
     );
 };
 
